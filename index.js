544c4c28eb4a30a9e846f9d8f16dbc873594839a
René Laplante <laplanter96@gmail.com>
2026-06-04 15:51:55 -0400

    Update edit and delete button with new colors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Array to store blog posts
 let posts = [];
